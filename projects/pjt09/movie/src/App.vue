dc656b2738ccb0b82d438e97dc2e9b907371dda4
marsa <m4ppom@gmail.com>
2019-11-08 09:26:59 +0900

    init proj 09

---
+++ projects/pjt09/movie/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <!-- 1-3. 호출하시오. 
+        필요한 경우 props를 데이터를 보내줍니다.
+      -->
+    </div>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+// 1-1. 저장되어 있는 MovieList 컴포넌트를 불러오고,
+
+
+export default {
+  name: 'app',
+  // 1-2. 아래에 등록 후
+  data() {
+    return {
+      // 활용할 데이터를 정의하시오.
+    }
+  },
+  mounted() {
+    // 0. mounted 되었을 때, 
+    // 1) 제시된 URL로 요청을 통해 data의 movies 배열에 해당 하는 데이터를 넣으시오. 
+    // 2) 제시된 URL로 요청을 통해 data의 genres 배열에 해당 하는 데이터를 넣으시오.
+    // axios는 위에 호출되어 있으며, node 설치도 완료되어 있습니다.
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
